4ff3b710346ca5b4032ca5e2b153123974170822
Julie Chu <julieychu@gmail.com>
2022-02-06 18:10:07 -0600

    beginning code to set up server and api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,68 @@
+var path = require("path");
+var fs = require("fs");
+
+// imported 'uuid' npm package for unique id
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = app => {
+
+// Set up notes variable
+fs.readFile("db/db.json","utf8", (err, data) => {
+
+    if (err) throw err;
+
+    const notes = JSON.parse(data)
+
+
+// Reading db.json file and returning all saved notesd as JSON
+app.get("/api/notes", function (req, res) {
+
+    console.log('Executing Get notes request');
+
+    return res.json(notes);
+});
+
+// Receiving and saving new note - with unique ID - to db.json file and returning new note to client
+app.post("/api/notes", function (req, res) {
+    // Extract new note from request body
+    let newNote = req.body;
+
+    console.log("Post request - new Note: " + newNote);
+    
+    //Assign unique id from 'uuid' npm package 
+    newNote.id = uuidv4()
+
+// Push new note into notes file, db.json
+    notes.push(newNote);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+
+        console.log("Successfully added new note: " + newNote.title);
+    });
+
+// Receiving request to delete note with specific ID and then updating db.json file with that note removed
+app.delete("/api/notes/:id", function (req, res) {
+    res.send('Got a DELETE request at /api/notes/:id')
+
+    let noteID = req.params.id.toString();
+
+    const newData = notes.filter (note => note.id.toString() !== noteID);
+    });
+
+    fs.writeFile("./db/db.json", JSON.stringify(newData), function (err) {
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+
+        console.log("Successfully deleted note with ID" + req.params.id);
+    
+    response.json(newData);
+
+});
+
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+const express = require('express');
+
+// Direct server to route files
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Create the express server
+const app = express();
+
+// Set up the PORT
+const PORT = process.env.PORT || 3001;
+
+// Middleware - setting up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Setting up routes for Express
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
